19668dce93fcf712ec4763ca67cec465301d3d89
Nicholas L <nicholas-l@users.noreply.github.com>
2022-12-01 05:16:06 +0000

    Formatting

--- src/day01.rs
+++ src/day01.rs
@@ -3,13 +3,10 @@ use std::io::BufRead;
 pub fn star_one(mut input: impl BufRead) -> usize {
     let mut buf = String::new();
     let _res = input.read_to_string(&mut buf);
-    buf
-        .split("\n\n")
-        .map(|elve|
-        {
-            elve.lines().map(|x| x.parse::<usize>().unwrap()).sum()
-        })
-        .max().unwrap()
+    buf.split("\n\n")
+        .map(|elve| elve.lines().map(|x| x.parse::<usize>().unwrap()).sum())
+        .max()
+        .unwrap()
 }
 
 pub fn star_two(mut input: impl BufRead) -> usize {
@@ -17,10 +14,8 @@ pub fn star_two(mut input: impl BufRead) -> usize {
     let _res = input.read_to_string(&mut buf);
     let mut elves = buf
         .split("\n\n")
-        .map(|elve|
-        {
-            elve.lines().map(|x| x.parse::<usize>().unwrap()).sum()
-        }).collect::<Vec<_>>();
+        .map(|elve| elve.lines().map(|x| x.parse::<usize>().unwrap()).sum())
+        .collect::<Vec<_>>();
     elves.sort();
     elves.iter().rev().take(3).sum()
 }
@@ -32,9 +27,9 @@ mod tests {
 
     #[test]
     fn test_star_one() {
-                assert_eq!(
-                    star_one(Cursor::new(
-                        b"1000
+        assert_eq!(
+            star_one(Cursor::new(
+                b"1000
 2000
 3000
 
@@ -48,16 +43,16 @@ mod tests {
 9000
 
 10000"
-                    )),
-                    24000
-                );
+            )),
+            24000
+        );
     }
 
     #[test]
     fn test_star_two() {
-                assert_eq!(
-                    star_two(Cursor::new(
-                        b"1000
+        assert_eq!(
+            star_two(Cursor::new(
+                b"1000
 2000
 3000
 
@@ -71,8 +66,8 @@ mod tests {
 9000
 
 10000"
-                    )),
-                    45000
-                );
+            )),
+            45000
+        );
     }
 }
